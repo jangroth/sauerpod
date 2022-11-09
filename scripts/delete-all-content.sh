470c5ccf3ed3983407d2d9eb4a79eac9b4bde8b8
Jan Groth <jan.groth.de@gmail.com>
2022-11-10 08:00:58 +1100

    Implementing DB delete

--- scripts/delete-all-content.sh
+++ scripts/delete-all-content.sh
@@ -2,19 +2,14 @@
 
 # exit 1 # really!?
 
-STORAGE_BUCKET_NAME=`aws cloudformation describe-stacks --stack-name sauerpod-long-lived --query "Stacks[0].Outputs[?OutputKey=='StorageBucketName'].OutputValue" --output text`
-STORAGE_TABLE_NAME=`aws cloudformation describe-stacks --stack-name sauerpod-long-lived --query "Stacks[0].Outputs[?OutputKey=='StorageTableName'].OutputValue" --output text`
+STORAGE_BUCKET_NAME=$(aws cloudformation describe-stacks --stack-name sauerpod-long-lived --query "Stacks[0].Outputs[?OutputKey=='StorageBucketName'].OutputValue" --output text)
+STORAGE_TABLE_NAME=$(aws cloudformation describe-stacks --stack-name sauerpod-long-lived --query "Stacks[0].Outputs[?OutputKey=='StorageTableName'].OutputValue" --output text)
 
-# aws s3 rm "s3://${STORAGE_BUCKET_NAME}" --recursive
+aws s3 rm "s3://${STORAGE_BUCKET_NAME}" --recursive
 
 aws dynamodb scan \
   --attributes-to-get EpisodeId TimestampUtc \
-  --table-name $STORAGE_TABLE_NAME --query "Items[*]" \
-  # use jq to get each item on its own line
-  | jq --compact-output '.[]' \
-  # replace newlines with null terminated so
-  # we can tell xargs to ignore special characters
-  | tr '\n' '\0' \
-  | xargs -0 -t -I keyItem \
-    # use the whole item as the key to delete (dynamo keys *are* dynamo items)
-    aws dynamodb delete-item --table-name $STORAGE_TABLE_NAME --key=keyItem
+  --table-name $STORAGE_TABLE_NAME --query "Items[*]" |
+  jq --compact-output '.[]' |
+  tr '\n' '\0' |
+  xargs -0 -t -I keyItem aws dynamodb delete-item --table-name $STORAGE_TABLE_NAME --key=keyItem
